438a9f645384f97597639b7d3e816a384ee38a72
Rodevp <rodriguezrodrigor12@gmail.com>
2023-01-17 17:25:28 -0500

    colors

---
+++ src/utils/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    purplePrimary: "rgb(244, 238, 255)",
+    purpleSecondary: "rgb(220, 214, 247)",
+    purpleLight: "rgb(166, 177, 225)",
+    purpleDark: "rgb(66, 72, 116)",
+    blueSky: "rgb(154, 220, 255)",
+    yellow: "rgb(255, 248, 154)"
+}
