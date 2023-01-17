02f940b8fd0379cbc3c0f1ac48769e1d33d942d4
Rodevp <rodriguezrodrigor12@gmail.com>
2023-01-17 18:58:12 -0500

    container styles

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -4,5 +4,6 @@ export const COLORS = {
     purpleLight: "rgb(166, 177, 225)",
     purpleDark: "rgb(66, 72, 116)",
     blueSky: "rgb(154, 220, 255)",
-    yellow: "rgb(255, 248, 154)"
+    yellow: "rgb(255, 248, 154)",
+    white: "rgb(251, 251, 251)"
 }
