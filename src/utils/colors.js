8d0a0d590b32d86297c72ca6c6370e2f8979a32f
Rodevp <rodriguezrodrigor12@gmail.com>
2023-01-20 16:37:37 -0500

    add color black an purple

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -5,5 +5,8 @@ export const COLORS = {
     purpleDark: "rgb(66, 72, 116)",
     blueSky: "rgb(154, 220, 255)",
     yellow: "rgb(255, 248, 154)",
-    white: "rgb(251, 251, 251)"
+    white: "rgb(251, 251, 251)",
+    gray: "rgb(234, 234, 234)",
+    purple: "rgb(145, 53, 234)",
+    black: "rgb(17,17,17)"
 }
