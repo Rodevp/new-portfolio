00904ed9ceb933da3072da998dd26be2ae91974a
Rodevp <rodriguezrodrigor12@gmail.com>
2023-03-04 14:06:01 -0500

    add colors

---
+++ src/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    black: "#111010",
+    gray: "#e5e5e5",
+    white: "#ffffff",
+    purple: "#865DFF",
+    purpleSecondary: "#AD7BE9",
+    purpleVeryLight: "#ECF2FF"
+}
